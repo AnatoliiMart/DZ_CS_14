c35fe57e5b205d6f61792e875fa6661aa64caea1
Anatolii <99074480+AnatoliiMart@users.noreply.github.com>
2023-04-26 21:38:11 +0200

    Добавьте файлы проекта.

---
+++ Company.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_CS_14
+{
+    public  class Company
+    {
+        public string CompanyName      { get; private set; }
+
+        public string directorName     { get; private set; }
+
+        public string CompanyAdress    { get; private set; }
+
+        public string buisnessProfile  { get; private set; }
+
+        public int amountOfWorkers     { get; private set; }
+
+        public DateTime dateOfCreation { get; private set; }
+
+        public Company()
+        {
+            
+        }
+        public Company(string CompanyName, string directorName, string CompanyAdress, 
+                    string businesProfile, int amountOfWorkers, DateTime dateOfCreation)
+        {
+            this.CompanyName     = CompanyName;
+            this.directorName    = directorName;
+            this.CompanyAdress   = CompanyAdress;
+            this.buisnessProfile = businesProfile;
+            this.amountOfWorkers = amountOfWorkers;
+            this.dateOfCreation  = dateOfCreation;
+        }
+        public override string ToString()
+        {
+            return $"Company name:\t\t{CompanyName}\n" +
+                   $"Director name:\t\t{directorName}\n" +
+                   $"Company adress:\t\t{CompanyAdress}\n" +
+                   $"Buisness profile:\t{buisnessProfile}\n" +
+                   $"Amount of workers:\t{amountOfWorkers}\n" +
+                   $"Date of creation:\t{dateOfCreation.ToShortDateString()}\n";
+        }
+    }
+    public class Companies : ICompany
+    {
+        private List<Company> companies = new List<Company>();
+        public Companies(List<Company> companies)
+        {
+            this.companies = companies;
+        }
+        public void CompaniesDirectorWhite()
+        {
+            Console.WriteLine("Companies wich directors name/surname is \'White\'");
+            foreach (var item in companies)
+            {
+                string[] str = item.directorName.Split(' ');
+                foreach (var item1 in str)
+                    if (item1 == "White")
+                        Console.WriteLine(item.ToString());
+            }
+        }
+
+        public void CompaniesMoreThan123DaysAgoCreated() // needs be fixed
+        {
+            var res = from item in companies
+                      where item.dateOfCreation.Month < (DateTime.Now.Month - 6)
+                      select item;
+            Console.WriteLine("Frims more tham 123 days ago created");
+            foreach (var item in res)
+                Console.WriteLine(item.ToString());
+            Console.WriteLine();
+        }
+
+        public void CompaniesMoreThanTwoYearsAgoCreated()
+        {
+            var res = from item in companies
+                      where item.dateOfCreation.Year < (DateTime.Now.Year - 2)
+                      select item;
+            Console.WriteLine("Frims more than 2 years ago created");
+            foreach (var item in res)
+                Console.WriteLine(item.ToString());
+            Console.WriteLine();
+        }
+
+        public void CompaniesPlacedInLondon()
+        {
+            Console.WriteLine("Companies placed in London");
+            foreach (var item in companies)
+            {
+                string[] str = item.CompanyAdress.Split(" ");
+                var res = from s in str
+                          where s == "London"
+                          select s;
+                foreach (var item1 in res)
+                    Console.WriteLine(item.ToString());
+                Console.WriteLine();
+            }
+        }
+
+        public void GetCompaniesInNameIsFood()
+        {
+            Console.WriteLine("Companies that have the word Food in their name ");
+            foreach (var item in companies)
+            {
+                string[] str = item.CompanyName.Split(" ");
+                var res = from s in str
+                          where s == "Food"
+                          select s;
+                foreach (var item1 in res)
+                    Console.WriteLine(item.ToString());
+                Console.WriteLine();
+            }
+        }
+
+        public void GetFrom100to300WorkersCompanies()
+        {
+            var res = from c in companies
+                      where c.amountOfWorkers >= 100 && c.amountOfWorkers <= 300
+                      select c;
+            Console.WriteLine("Companies what have from 100 to 300 workers");
+            foreach (var item in res)
+                Console.WriteLine(item.ToString());
+            Console.WriteLine();
+        }
+
+        public void GetMarketingAndItCompanies()
+        {
+            Console.WriteLine("Companies with marketing and IT profile");
+            var res = from c in companies
+                      where c.buisnessProfile.Contains("Marketing") && c.buisnessProfile.Contains("IT")
+                      select c;
+            foreach (var item in res)
+                Console.WriteLine(item.ToString() + "\n");
+            Console.WriteLine();
+        }
+
+        public void GetMarketingCompanies()
+        {
+           var res = from c in companies
+                     where c.buisnessProfile == "Marketing"
+                     select c;
+            foreach (var item in res)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        public void GetMoreThan100WorkersCompanies()
+        {
+            var res = from c in companies
+                      where c.amountOfWorkers > 100
+                      select c;
+            Console.WriteLine("Companies what have more than 100 workers");
+            foreach (var item in res)
+                Console.WriteLine(item.ToString());
+            Console.WriteLine();
+        }
+
+        public void RequestFromHELL()
+        {
+            var res = from c in companies
+                      where c.CompanyName.Contains("White") && c.directorName.Contains("Black")
+                      select c;
+            Console.WriteLine("Companies what in company name contains \'White\' and director name/surname contains \'Black\'");
+            foreach (var item in res)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        public void ShowAllCompanies()
+        {
+           foreach (var item in companies)
+                Console.WriteLine(item.ToString() + "\n");
+
+        }
+    }
+    public interface ICompany
+    {
+        void ShowAllCompanies();
+        void GetCompaniesInNameIsFood();
+        void GetMarketingCompanies();
+        void GetMarketingAndItCompanies();
+        void GetMoreThan100WorkersCompanies();
+        void GetFrom100to300WorkersCompanies();
+        void CompaniesPlacedInLondon();
+        void CompaniesDirectorWhite();
+        void CompaniesMoreThanTwoYearsAgoCreated();
+        void CompaniesMoreThan123DaysAgoCreated();
+        void RequestFromHELL();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DZ_CS_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICompany companies = new Companies(new List<Company>
+            {
+               new Company("A", "Bobby White", "London", "IT", 10, new DateTime(2023, 2, 15)),
+
+               new Company("OD Food", "2", "Paris", "Marketing", 120, new DateTime(2022, 12, 25)),
+
+               new Company("C", "4", "Odesa", "Marketing and IT", 100, new DateTime(2022, 3, 10)),
+
+               new Company("French Food", "3", "Krakow", "Restourans", 300, new DateTime(2020, 11, 28)),
+
+               new Company("White Horse", "David Black", "London", "Logistic", 150, new DateTime(2010, 1, 17)),
+
+               new Company("F", "White Bobby", "Warsaw", "Travelling", 45, new DateTime(2005, 6, 11)),
+
+               new Company("G", "7", "Kyiv", "IT", 30, new DateTime(2016, 7, 31)),
+            });
+            companies.ShowAllCompanies();
+            companies.RequestFromHELL();
+            companies.CompaniesPlacedInLondon();
+           
+          
+        }
+    }
+}
